544e642a420fda3268c5e91c8ee50b1de255999f
Tadhg Lewis <tadhg.lewis@outlook.com>
2023-08-16 21:44:16 +1000

    Init

---
+++ index.mjs
@@ -0,0 +1,44 @@
+import http from "http";
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import cors from "@koa/cors";
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { koaMiddleware } from "@as-integrations/koa";
+import { InMemoryLRUCache } from "@apollo/utils.keyvaluecache";
+
+// The GraphQL schema
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const app = new Koa();
+const httpServer = http.createServer(app.callback());
+
+// Set up Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+
+app.use(cors());
+app.use(bodyParser());
+app.use(
+  koaMiddleware(server, {
+    context: async ({ ctx }) => ({ token: ctx.headers.token }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000`);
